8074d1cbd555cb15b72c107019ddce7e6e0e2742
Daniel Gillespie <dan@danielgillespie.net>
2018-05-12 09:17:14 -0700

    added better integration with windows screensaver manager. screensaver exits on mouse move or keyboard press. more minor addons

--- src/main/java/eagleview/App.java
+++ src/main/java/eagleview/App.java
@@ -1,18 +1,23 @@
 package eagleview;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.InvalidationListener;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
+import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
@@ -35,43 +40,81 @@ public class App extends Application
     private static boolean isConfigurationMode = false;
     private static boolean isDialogSelectorPreviewMode = false;
     private static String previewWindowHandle = null;
+    private static boolean isFirstRunMouse = true;
+    private static Point2D mouseLocation = null;
+
+    private static String[] arguments = null;
 
     private TilePane grid;
 
-    @Override
-    public void start (Stage primaryStage) throws Exception {
-       if(isFullscreenMode) {
-           startScreensaver(primaryStage);
-       } else if(isConfigurationMode) {
-           startConfigMode(primaryStage);
-       } else {
-           isWindowPreviewMode = true;
-           startScreensaver(primaryStage);
-       }
-    }
-
     public static void main( String[] args ) throws Exception {
-        for(int i = 0; i <  args.length; i++) {
-            String arg = args[i];
-
-            if(arg.equals("/s")) {
-                isFullscreenMode = true;
-            } else if(arg.equals("/c")) {
-                isConfigurationMode = true;
-            } else if(arg.equals("/p")) {
-                isDialogSelectorPreviewMode = true;
-
-                if(args.length >= i+1) {
-                    previewWindowHandle = args[i+1];
-                }
-            }
-        }
-
+        arguments = args;
         Application.launch (args);
     }
 
-    private void startConfigMode(Stage primaryStage) throws Exception {
+    @Override
+    public void start (Stage primaryStage) throws Exception {
+        if(arguments.length > 0) {
+//            String argFull = "";
+//            for(String arg : arguments) {
+//                argFull += String.format("ARG: %s", arg);
+//            }
+//
+//            Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
+//            alert2.setContentText(argFull);
+//            alert2.showAndWait();
 
+            switch(arguments[0].toLowerCase().substring(0, 2)) {
+                case "/s":
+                    isFullscreenMode = true;
+                    break;
+                case "/c":
+                    // Windows screensaver manager seems to pass /c:123456 (hwnd?) instead of just /c
+                    isConfigurationMode = true;
+                    break;
+                case "/p":
+                    if(arguments.length > 1) {
+                        isDialogSelectorPreviewMode = true;
+                        previewWindowHandle = arguments[1];
+                    } else {
+                        Alert alert = new Alert(Alert.AlertType.WARNING, "Preview Window Handle Required!", ButtonType.OK);
+
+                        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+                        stage.setAlwaysOnTop(true);
+                        alert.show();
+                    }
+                    break;
+            }
+        }
+
+
+
+        primaryStage.setOnCloseRequest(e -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+        if(isFullscreenMode) {
+            startScreensaver(primaryStage);
+        } else if(isConfigurationMode) {
+            startConfigMode(primaryStage);
+        } else if(isDialogSelectorPreviewMode) {
+            // TBD do something
+            // ...
+        } else {
+            isWindowPreviewMode = true;
+            startScreensaver(primaryStage);
+        }
+    }
+
+    private void startConfigMode(Stage primaryStage) throws Exception {
+        primaryStage.setTitle ("Config - Eagle View");
+
+        Label labelTest = new Label("Hello, Configuration!");
+        Scene sceneMain = new Scene(labelTest, 800, 600);
+
+        primaryStage.setScene(sceneMain);
+        primaryStage.show();
     }
 
     private void startScreensaver(Stage primaryStage) throws Exception {
@@ -118,22 +161,12 @@ public class App extends Application
             }
         });
 
-        // TBD: REMOVE BEFORE GETTING TO PRODUCTION (ONLY USE IN DEV MODE)
-        if(!isFullscreenMode) {
-            sceneMain.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
-                @Override
-                public void handle(KeyEvent t) {
-                    if(t.getCode()==KeyCode.ESCAPE)
-                    {
-                        primaryStage.close();
-                    }
-                }
-            });
-        }
-
         if(isFullscreenMode) {
             primaryStage.initStyle(StageStyle.UNDECORATED);
             primaryStage.setFullScreen(true);
+            primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH); // Disable fullscreen exit message
+
+            sceneMain.setCursor(Cursor.NONE);
 
             Screen screen = Screen.getPrimary();
             Rectangle2D bounds = screen.getVisualBounds();
@@ -142,6 +175,28 @@ public class App extends Application
             primaryStage.setY(bounds.getMinY());
             primaryStage.setWidth(bounds.getWidth());
             primaryStage.setHeight(bounds.getHeight());
+
+            sceneMain.addEventFilter(MouseEvent.MOUSE_MOVED , new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    if(isFirstRunMouse) {
+                        isFirstRunMouse = false;
+                        mouseLocation = new Point2D(mouseEvent.getX(), mouseEvent.getY());
+                    } else {
+                        if(mouseLocation != null) {
+                            if(mouseEvent.getX() >= mouseLocation.getX()+20 || mouseEvent.getX() >= mouseLocation.getX()-20
+                                    || mouseEvent.getY() >= mouseLocation.getY()+20 || mouseEvent.getY() >= mouseLocation.getY()-20) {
+
+                                primaryStage.close();
+                            }
+                        }
+                    }
+                }
+            });
+
+            sceneMain.setOnKeyPressed(event -> {
+                primaryStage.close();
+            });
         }
 
         primaryStage.setScene(sceneMain);

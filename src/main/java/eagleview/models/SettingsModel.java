e5cad9691eb528d4fb48270b2b1aec63e44a08ec
Dan Gillespie <dan@danielgillespie.net>
2018-05-18 11:48:26 -0700

    cache directories created now if they dont exist on app start

--- src/main/java/eagleview/models/SettingsModel.java
+++ src/main/java/eagleview/models/SettingsModel.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SettingsModel {
-    private static String baseAppDir; // where the app dir is located
-    private static String videoCollectionDir; // where the video cache dir is located
+    public String baseAppDir; // where the app dir is located
+    public String videoCollectionDir; // where the video cache dir is located
 
-    public static int columnCount;
-    public static int rowCount;
-    public static List<VideoItemModel> videoCollection;
+    public int columnCount;
+    public int rowCount;
+    public List<VideoItemModel> videoCollection;
 
     public SettingsModel() throws Exception {
         if(videoCollection == null) {
@@ -21,10 +21,12 @@ public class SettingsModel {
 
         if(StringUtils.isEmpty(baseAppDir)) {
             baseAppDir = Utilities.getApplicationPath();
+            System.out.println(String.format("Application Base Directory: %s", baseAppDir));
         }
 
         if(StringUtils.isEmpty(videoCollectionDir)) {
             videoCollectionDir = String.format("%s/cache/videos", baseAppDir);
+            System.out.println(String.format("Video Cache Directory: %s", videoCollectionDir));
         }
 
         // ... TBD change

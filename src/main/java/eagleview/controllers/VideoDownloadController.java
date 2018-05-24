9b060a6690294668e9dcff721d344fcc9334ed19
Dan Gillespie <dan@danielgillespie.net>
2018-05-24 10:56:45 -0700

    added modal video popup for selected video download dialog

---
+++ src/main/java/eagleview/controllers/VideoDownloadController.java
@@ -0,0 +1,13 @@
+package eagleview.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VideoDownloadController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Config window initialized!");
+    }
+}

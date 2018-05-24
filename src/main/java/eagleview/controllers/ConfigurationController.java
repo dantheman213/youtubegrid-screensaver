57db0cda56e20f06fd38e61d5349d8fc72b19f6c
Dan Gillespie <dan.gillespie@lootcrate.com>
2018-05-24 10:56:45 -0700

    added modal video popup for selected video download dialog

--- src/main/java/eagleview/controllers/ConfigurationController.java
+++ src/main/java/eagleview/controllers/ConfigurationController.java
@@ -5,14 +5,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextInputDialog;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import org.apache.commons.lang.StringUtils;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class ConfigurationController implements Initializable {
@@ -25,26 +24,24 @@ public class ConfigurationController implements Initializable {
     @FXML
     private Button buttonVideoRemove;
 
-    // internal data
-    private static List<String> importVideoQueue;
-
-    public ConfigurationController() throws Exception {
-        if(importVideoQueue == null) {
-            importVideoQueue = new ArrayList<String>();
-        }
-    }
-
     @FXML
     private void handleButtonVideoAddClicked(ActionEvent event) throws Exception {
         TextInputDialog dialog = new TextInputDialog();
         dialog.setTitle("What video would you like to add?");
         dialog.setContentText("Please provide a YouTube URL to add to your video list.");
-        dialog.initOwner(buttonVideoAdd.getScene().getWindow());
+
+        Window parentWindow = buttonVideoAdd.getScene().getWindow();
+        dialog.initOwner(parentWindow);
 
         dialog.showAndWait().ifPresent(x -> {
             if(StringUtils.isNotBlank(x)) {
                 System.out.println(x);
-                importVideoQueue.add(x);
+                // TBD ...
+                try {
+                    App.windowManager.launchVideoDownloadModal(parentWindow);
+                } catch(Exception ex) {
+                    ex.printStackTrace();
+                }
 
             }
         });
@@ -60,12 +57,12 @@ public class ConfigurationController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        listVideo.getItems().add("YouTube Video 1");
-        listVideo.getItems().add("YouTube Video 2");
-        listVideo.getItems().add("YouTube Video 3");
-        listVideo.getItems().add("YouTube Video 4");
-        listVideo.getItems().add("YouTube Video 5");
-        listVideo.getItems().add("YouTube Video 6");
+        //listVideo.getItems().add("YouTube Video 1");
+        //listVideo.getItems().add("YouTube Video 2");
+        //listVideo.getItems().add("YouTube Video 3");
+        //listVideo.getItems().add("YouTube Video 4");
+        //listVideo.getItems().add("YouTube Video 5");
+        //listVideo.getItems().add("YouTube Video 6");
 
         System.out.println("Config window initialized!");
     }
--- src/main/java/eagleview/data/WindowManager.java
+++ src/main/java/eagleview/data/WindowManager.java
@@ -5,18 +5,34 @@ import eagleview.controllers.ScreensaverController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class WindowManager {
 
     public void launchConfigWindow(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(ConfigurationController.class.getClassLoader().getResource("views/vwConfiguration.fxml"));
+        Parent root = FXMLLoader.load(WindowManager.class.getClassLoader().getResource("views/vwConfiguration.fxml"));
 
         stage.setTitle ("Config - Eagle View");
         stage.setScene(new Scene(root, 800, 600));
 
         //stage.setAlwaysOnTop(true);
         stage.show();
+    }
+
+    public void launchVideoDownloadModal(Window parentWindow) throws Exception {
+        Parent root = FXMLLoader.load(WindowManager.class.getClassLoader().getResource("views/modalVideoDownload.fxml"));
+
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle ("Downloading Video...");
+        dialogStage.setScene(new Scene(root, 320, 240));
+        dialogStage.setX(parentWindow.getX() + 300);
+        dialogStage.setY(parentWindow.getY() + 300);
+        dialogStage.initOwner(parentWindow);
+        dialogStage.initModality(Modality.APPLICATION_MODAL);
+        //dialogStage.setAlwaysOnTop(true);
+        dialogStage.show();
     }
 
     public void launchScreensaver(Stage stage) throws Exception {

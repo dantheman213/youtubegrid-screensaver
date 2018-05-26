74bb0fad680c08d22349a4d94d135cbed32a98a7
Daniel Gillespie <dan@danielgillespie.net>
2018-05-26 09:21:24 -0700

    modal dialog closes on error or on completition now. alerts are showing when being requested.

--- src/main/java/eagleview/controllers/VideoDownloadController.java
+++ src/main/java/eagleview/controllers/VideoDownloadController.java
@@ -3,9 +3,11 @@ package eagleview.controllers;
 import eagleview.App;
 import eagleview.data.Config;
 import eagleview.data.Utilities;
+import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.io.BufferedReader;
@@ -32,16 +34,25 @@ public class VideoDownloadController implements Initializable {
         if (!f.exists()) {
             System.out.println("Unable to locate 'youtube-dl' binary! Please reinstall.");
 
-            closeWindow();
-            Utilities.showSimpleAlert("Unable to locate 'youtube-dl' binary! Please reinstall.");
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    Alert alert = Utilities.showSimpleAlert("Unable to locate 'youtube-dl' binary! Please reinstall.");
+                    alert.showAndWait();
+                    closeWindow();
+                }
+            });
+
             return;
         }
-
 
         Task<Void> task = new Task<Void>() {
             @Override
             protected Void call() {
-                BufferedReader bufferedReader = null;
+                BufferedReader bufferedReaderStdOut = null;
+                BufferedReader bufferedReaderStdErr = null;
+
+                boolean isError = false;
 
                 try {
                     System.out.println("Starting video download thread...");
@@ -49,41 +60,65 @@ public class VideoDownloadController implements Initializable {
                     String[] cmdArgs = {
                             App.config.settings.youtubeDlBin,
                             "-o %(title)s.%(ext)s",
-                            //String.format("-o \"%s%s%s\"", App.config.settings.videoCollectionDir, File.separator, "%(title)s.%(ext)s"),
                             "-f worstvideo[ext=mp4]",
-                            //"--no-certificate-check",
                             url
                     };
 
                     ProcessBuilder procBuilder = new ProcessBuilder(cmdArgs);
                     procBuilder.directory(new File(App.config.settings.videoCollectionDir));
-                    procBuilder.redirectErrorStream(true);
-
+                    //procBuilder.redirectErrorStream(true);
                     Process proc = procBuilder.start();
 
-                    InputStream inputStream = proc.getInputStream();
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                    bufferedReader = new BufferedReader(inputStreamReader);
+                    bufferedReaderStdOut = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                    bufferedReaderStdErr = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
 
                     System.out.println("Checking for program response...");
-                    String consoleLine = bufferedReader.readLine();
-                    while(consoleLine != null) {
-                        System.out.println(consoleLine);
-                        consoleLine  = bufferedReader.readLine();
+
+                    String stdOutLine = bufferedReaderStdOut.readLine();
+                    String stdErrLine = bufferedReaderStdErr.readLine();
+                    while(stdOutLine != null || stdErrLine != null) {
+                        if(stdOutLine != null) {
+                            System.out.println(stdOutLine);
+                        }
+
+                        if(stdErrLine != null) {
+                            System.out.println(stdErrLine);
+                            isError = true;
+                        }
+
+                        stdOutLine  = bufferedReaderStdOut.readLine();
+                        stdErrLine = bufferedReaderStdErr.readLine();
                     }
 
                     proc.waitFor();
                 } catch(Exception ex) {
                     // TBD
+                    isError = true;
                     ex.printStackTrace();
                 } finally {
-                    System.out.println("Finished.. closing dialog.");
-                    if(bufferedReader != null) {
-                        try {bufferedReader.close();} catch(Exception ex) {}
+                    System.out.println("Finished.. closing buffer.");
+                    if(bufferedReaderStdOut != null) {
+                        try {bufferedReaderStdOut.close();} catch(Exception ex) {}
                     }
                 }
 
-                closeWindow();
+                // Create an on-the-fly custom Runnable that can receive data input into its thread
+                class CloseWindowTask implements Runnable {
+                    boolean isError;
+                    public CloseWindowTask(boolean error) {
+                        isError = error;
+                    }
+
+                    public void run() {
+                        System.out.println("Close dialog window...");
+                        closeWindow();
+
+                        Alert alert = Utilities.showSimpleAlert((isError ? "An error occurred!" : "Completed Successfully!"));
+                        alert.show();
+                    }
+                }
+                Platform.runLater(new CloseWindowTask(isError));
+
                 return null;
             }
         };

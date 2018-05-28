1ef05dcbcea24d8fdc15a9a7ab207a43725b75b3
Daniel Gillespie <dan@danielgillespie.net>
2018-05-28 08:16:02 -0700

    split out tasks into each own file for video download stuff

--- src/main/java/eagleview/controllers/VideoDownloadController.java
+++ src/main/java/eagleview/controllers/VideoDownloadController.java
@@ -3,6 +3,9 @@ package eagleview.controllers;
 import eagleview.App;
 import eagleview.data.Config;
 import eagleview.data.Utilities;
+import eagleview.tasks.ShowDialogCompleteTask;
+import eagleview.tasks.UpdateWindowLogTask;
+import eagleview.tasks.VideoDownloadTask;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
@@ -50,107 +53,7 @@ public class VideoDownloadController implements Initializable {
             return;
         }
 
-        Task<Void> task = new Task<Void>() {
-            @Override
-            protected Void call() {
-                BufferedReader bufferedReaderStdOut = null;
-                BufferedReader bufferedReaderStdErr = null;
-
-                boolean isError = false;
-
-                try {
-                    System.out.println("Starting video download thread...");
-
-                    String[] cmdArgs = {
-                            App.config.settings.youtubeDlBin,
-                            "-o %(title)s.%(ext)s",
-                            "-f worstvideo[ext=mp4]",
-                            url
-                    };
-
-                    ProcessBuilder procBuilder = new ProcessBuilder(cmdArgs);
-                    procBuilder.directory(new File(App.config.settings.videoCollectionDir));
-                    Process proc = procBuilder.start();
-
-                    bufferedReaderStdOut = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-                    bufferedReaderStdErr = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
-
-                    System.out.println("Checking for program response...");
-
-                    String stdOutLine = bufferedReaderStdOut.readLine();
-                    String stdErrLine = bufferedReaderStdErr.readLine();
-                    while(stdOutLine != null || stdErrLine != null) {
-                        String logLine = "";
-                        if(stdOutLine != null) {
-                            System.out.println(stdOutLine);
-                            logLine += stdOutLine + "\n";
-                        }
-
-                        if(stdErrLine != null) {
-                            System.out.println(stdErrLine);
-                            logLine += stdErrLine + "\n";
-                            isError = true;
-                        }
-
-                        // On-the-fly class to pass data into runnable
-                        class UpdateWindowLog implements Runnable {
-                            private String line;
-
-                            public UpdateWindowLog(String logLine) {
-                                line = logLine;
-                            }
-
-                            @Override
-                            public void run() {
-                                textLog.setText(textLog.getText() + line);
-                            }
-                        }
-                        Platform.runLater(new UpdateWindowLog(logLine));
-
-                        stdOutLine = bufferedReaderStdOut.readLine();
-                        stdErrLine = bufferedReaderStdErr.readLine();
-                    }
-
-                    proc.waitFor();
-                } catch(Exception ex) {
-                    // TBD
-                    isError = true;
-                    ex.printStackTrace();
-                } finally {
-                    System.out.println("Finished.. closing buffer.");
-                    if(bufferedReaderStdOut != null) {
-                        try {bufferedReaderStdOut.close();} catch(Exception ex) {}
-                    }
-                }
-
-                // Create an on-the-fly custom Runnable that can receive data input into its thread
-                class ShowDialogCompleteTask implements Runnable {
-                    private boolean isError;
-
-                    public ShowDialogCompleteTask(boolean error) {
-                        isError = error;
-                    }
-
-                    @Override
-                    public void run() {
-                        Alert alert;
-
-                        if(isError) {
-                            alert = Utilities.showSimpleAlert("An error occurred!");
-                        } else {
-                            alert = Utilities.showSimpleAlert("Completed Successfully!");
-                        }
-
-                        alert.show();
-                    }
-                }
-                Platform.runLater(new ShowDialogCompleteTask(isError));
-
-                return null;
-            }
-        };
-
-        // Execute the task described above
+        Task<Void> task = new VideoDownloadTask(url, textLog);
         Thread thread = new Thread(task);
         thread.setDaemon(true);
         thread.start();
